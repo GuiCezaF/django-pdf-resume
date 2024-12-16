c6a48946f83a99301fff1766ae8fe59f32026750
Guilherme.Felipe <guilherme.felipe@retta.com.br>
2024-12-16 08:37:05 -0300

    feat: implementando model de resumo e admin rodando sua primeira migração

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from core.models import Resume
 
-# Register your models here.
+
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('file_name', 'is_resumed', 'created_at', 'updated_at')
+
+    fields = ('file_name', 'is_resumed', 'created_at', 'updated_at')
+
+
+admin.site.register(Resume, ResumeAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator
 
-# Create your models here.
+class Resume(models.Model):
+  
+  file_name = models.CharField("Nome do arquivo", max_length=150, blank=False, null=False)
+  resume = models.TextField("Resumo feito", validators=[MaxLengthValidator(3000)], blank=True, null=True)
+  is_resumed = models.BooleanField("Esse pdf ja foi resumido?", default=False)
+  created_at = models.DateTimeField("Data de criação", auto_now_add=True ,blank=False, null=False)
+  updated_at = models.DateTimeField("Data de atualização", auto_now=True ,blank=False, null=False)
+  
+  def __str__(self):
+    return self.file_name
