fda1ebdb901b364cd5be5a3528d56516eedcce80
Guilherme.Felipe <guilherme.felipe@retta.com.br>
2024-12-17 09:24:03 -0300

    feat: validando erros do ollama service

--- core/services/ollama_service.py
+++ core/services/ollama_service.py
@@ -1,26 +1,35 @@
-import requests
+import os
 import json
+import requests
 
 
 class OllamaService:
-    url = "http://localhost:11434/api/chat"
-
-    data = {
-        "model": "llama3.2",
-        "messages": [{"role": "user"}]
-    }
+    def __init__(self, url=None):
+        self.url = url or os.getenv(
+            "OLLAMA_SERVICE_URL", "http://localhost:11434/api/chat")
 
     def send_message(self, message: str):
-        self.data["messages"][0]["content"] = message
-        content = ""
+        data = {
+            "model": "llama3.2",
+            "messages": [{"role": "user", "content": message}]
+        }
+        messages = []
 
-        with requests.post(self.url, json=self.data, stream=True) as response:
-            for line in response.iter_lines():
-                if line: 
-                    try:
-                        parsed_line = json.loads(line.decode("utf-8"))
-                        content += parsed_line.get("message",{}).get("content", "")
-                    except json.JSONDecodeError:
-                        continue
+        try:
+            with requests.post(self.url, json=data, stream=True) as response:
+                response.raise_for_status()
 
-        return content
+                for line in response.iter_lines():
+                    if line:
+                        try:
+                            parsed_line = json.loads(line.decode("utf-8"))
+                            content = parsed_line.get(
+                                "message", {}).get("content", "")
+                            if content:
+                                messages.append(content)
+                        except json.JSONDecodeError as e:
+                            print(f"Erro ao decodificar linha: {e}")
+        except requests.RequestException as e:
+            raise RuntimeError(f"Erro ao conectar ao servidor Ollama: {str(e)}")
+
+        return ''.join(messages)
